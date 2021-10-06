dc729190f6ceb7b4cb176ee14c241e4e117bd975
Damien Demolin <damien.demolin@gmail.com>
2021-10-07 01:17:08 +0200

    Ajout des produits sur la page principale

--- front/js/script.js
+++ front/js/script.js
@@ -6,3 +6,18 @@ const getProduct = async () => {
     .then((res) => res.json())
     .then((data) => products = data );
 };
+const productDisplay = async () => {
+    await getProduct ();
+
+    items.innerHTML = products.map((product) => 
+    `
+    <a href="./product.html?id=${product._id}">
+    <article>
+    <img src="${product.imageUrl}" alt="${product.altTxt}, ${product.name}">
+    <h3 class="productName">${product.name}</h3>
+    <p class="productDescription">${product.description}</p>
+    </article>
+    </a>
+    `)
+}
+productDisplay();
