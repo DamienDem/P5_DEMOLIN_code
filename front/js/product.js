e501faea38b6cf8c3f8d784fb88f320cd4bc2af4
Damien Demolin <damien.demolin@gmail.com>
2021-10-11 15:15:38 +0200

    affichage des produits sur la page product

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,12 @@
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 let product = [];
+const productImage = document.querySelector('.item__img');
+const produtcTitle = document.getElementById('title');
+const produtcPrice = document.getElementById('price');
+const productDescription = document.getElementById('description');
+const colorSelect = document.getElementById('colors');
+console.log(productImage,produtcTitle,produtcPrice,productDescription,colorSelect);
 
 // Récupération des id dans l'URL
 const getProduct = async () => {
@@ -10,3 +16,15 @@ const getProduct = async () => {
     console.log(product)
 };
 getProduct();
+const productDisplay = async () => {
+    await getProduct();
+    productImage.innerHTML = `<img src= "${product.imageUrl}" alt="${product.altTxt}, ${product.name}">`;
+    produtcTitle.innerHTML = `${product.name}`;
+    produtcPrice.innerHTML = `${product.price}`;
+    productDescription.innerHTML = `${product.description}`;
+    colorSelect.innerHTML = product.colors.map((color) =>
+     `
+     <option value="${color}"> ${color} </option>S
+    `);
+};
+productDisplay();
