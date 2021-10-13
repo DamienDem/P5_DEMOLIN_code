d228888cb3f8cbafb37cf0a0f58fde81987b6982
Damien Demolin <damien.demolin@gmail.com>
2021-10-13 09:26:58 +0200

    ajout des conditions d'ajout au panier

--- front/js/product.js
+++ front/js/product.js
@@ -7,7 +7,8 @@ const produtcPrice = document.getElementById('price');
 const productDescription = document.getElementById('description');
 const colorSelect = document.getElementById('colors');
 const addToBasket = document.getElementById('addToCart');
-const quantity = document.getElementById("quantity");
+const quantitySelect = document.getElementById("quantity");
+let cart = [];
 
 // Récupération du produit avec l'ID
 const getProduct = async () => {
@@ -33,5 +34,23 @@ const productDisplay = async () => {
 };
 productDisplay();
 
+// Si le local storage contient une valeur la récupérer et l'jouter dans l'array cart
+if (localStorage.getItem('cart') !== null) {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    console.log('localstorage récupéré');
+};
+
+// Si le panier ne contient pas l'id de notre produit ajoute le produit au panier
+if(cart.every((id) => id != `${id}`)){
+    cart.push(product);
+    console.log(cart);
+    console.log('ajoute un nouveau produit au panier');
+};
+
+// Si l'ID et la couleur sont déja présent dans le panier ajoute la quantité sélectionné au panier
+if(cart.every((id) => id == `${id}`) && cart.every((color) => color == `${color}`)) {
+    console.log(cart);
+    console.log('ajoute une nouelle couleur');
+};
 
 
