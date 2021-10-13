73616dbe00db552340b9b97a318b030714ef70c2
Damien Demolin <damien.demolin@gmail.com>
2021-10-13 09:03:05 +0200

    Affichage du produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,13 @@
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 let product = [];
+const productImage = document.querySelector('.item__img');
+const produtcTitle = document.getElementById('title');
+const produtcPrice = document.getElementById('price');
+const productDescription = document.getElementById('description');
+const colorSelect = document.getElementById('colors');
+const addToBasket = document.getElementById('addToCart');
+const quantity = document.getElementById("quantity");
 
 // Récupération du produit avec l'ID
 const getProduct = async () => {
@@ -10,5 +17,21 @@ const getProduct = async () => {
     console.log(product)
 };
 getProduct();
+
+// Affichage du produit
+const productDisplay = async () => {
+    await getProduct();
+    productImage.innerHTML = `<img src= "${product.imageUrl}" alt="${product.altTxt}, ${product.name}">`;
+    produtcTitle.innerHTML = `${product.name}`;
+    produtcPrice.innerHTML = `${product.price}`;
+    productDescription.innerHTML = `${product.description}`;
+    colorSelect.innerHTML = product.colors.map((color) =>
+     `
+     <option value="${color}"> ${color} </option>
+    `)
+    .join("");
+};
+productDisplay();
+
 
 
