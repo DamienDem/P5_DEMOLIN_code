0fc5aa808c2e183218f5d214daabfa8d41375775
Damien Demolin <damien.demolin@gmail.com>
2021-10-18 17:09:39 +0200

    mise en place de la quantité et du prix total

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,24 @@
 
 const cartItems = document.getElementById('cart__items');
 let cart = JSON.parse(localStorage.cart);
-
+`<div class="cart__price">
+<p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>`
+const totalQuantity = document.getElementById('totalQuantity');
+const totalPrice = document.getElementById('totalPrice');
+const total = async () => {
+  await displayProductInCart();
+  // console.log(arrayNumber.reduce((x, y) => x + y)); additione tout les éléments d'un tableau
+  let totalItems = 0;
+  let priceItems = 0;
+  for(let i = 0; i<cart.length; i++)
+  {
+    totalItems += cart[i].quantityTotal
+    priceItems = priceItems + cart[i].quantityTotal * cart[i].price;
+  }
+  totalQuantity.textContent = totalItems;
+  totalPrice.textContent = priceItems;
+}
+total();
 function displayProductInCart() {
  // console.log(cart);
   cartItems.innerHTML = cart.map((product) => `
@@ -28,6 +45,7 @@ function displayProductInCart() {
               </article>
     `
   ).join("");
+  //total();
   
 };
 
@@ -41,17 +59,17 @@ const quantityChange = async () => {
       cart[i].quantityTotal = quantitySelect;
       let qte = itemQuantity[i].previousElementSibling;
       qte.textContent = "Qté: " +quantitySelect;    
-
       localStorage.setItem('cart' , JSON.stringify(cart));
      // console.log(cart);
     }
+    total();
   });
 } 
 quantityChange();
 const deleteItem = document.getElementsByClassName('deleteItem');
 const deleteProduct = async () => {
   await displayProductInCart();
-  console.log(cart);
+  //console.log(cart);
   for(let i = 0 ; i < cart.length ; i++)
   {
     const cartItem = document.getElementsByClassName('cart__item');
@@ -73,6 +91,7 @@ const deleteProduct = async () => {
         console.log(i);
       }
      localStorage.setItem('cart' , JSON.stringify(cart));
+     total();
     }); 
   }
 };
