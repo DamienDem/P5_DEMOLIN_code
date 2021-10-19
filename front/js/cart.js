4ea3193573260791f6161bbc0114de65be912b2b
Damien Demolin <damien.demolin@gmail.com>
2021-10-19 11:40:48 +0200

    création du formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,10 @@
 
 const cartItems = document.getElementById('cart__items');
 let cart = JSON.parse(localStorage.cart);
-`<div class="cart__price">
-<p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>`
 const totalQuantity = document.getElementById('totalQuantity');
 const totalPrice = document.getElementById('totalPrice');
 const total = async () => {
   await displayProductInCart();
-  // console.log(arrayNumber.reduce((x, y) => x + y)); additione tout les éléments d'un tableau
   let totalItems = 0;
   let priceItems = 0;
   for(let i = 0; i<cart.length; i++)
@@ -44,9 +41,7 @@ function displayProductInCart() {
                 </div>
               </article>
     `
-  ).join("");
-  //total();
-  
+  ).join(""); 
 };
 
 const quantityChange = async () => {
@@ -96,4 +91,130 @@ const deleteProduct = async () => {
   }
 };
 deleteProduct();
+
+ //-----------------------
+ // FORMULAIRE
+ //---------------------
+
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll(
+  'input[type="text"] , input[type="email"]'
+);
+console.log(inputs);
+console.log(form);
+let name,firstName,address,city, email;
+const container = document.querySelector(".cart__order__form__question");
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "firstName":
+        firstNameChecker(e.target.value);
+        break;
+        case "lastName":
+        lastNameChecker(e.target.value);
+        break;
+        case "address":
+        addressChecker(e.target.value);
+        break;
+      case "city":
+        cityChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      default:
+        nul;
+    }
+  });
+});
+
+const errorDisplay = (tag, message) => {
+  const errorMessage = document.getElementById(tag +"ErrorMsg");
+  errorMessage.textContent = message;
+};
+
+const firstNameChecker = (value) => {
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("firstName", "Le Prénom doit faire entre 3 et 20 caractères");
+    firstName = null;
+  } else if (!value.match(/^[a-zA-Z. -]*$/)) {
+    errorDisplay("firstName","Le Prénom ne doit pas contenir de caractères spéciaux");
+    firstName = null;
+  } else { 
+    errorDisplay("firstName", "");
+    firstName = value;
+  }
+};
+const lastNameChecker = (value) => {
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("lastName", "Le Nom doit faire entre 3 et 20 caractères");
+    firstName = null;
+  } else if (!value.match(/^[a-zA-Z. -]*$/)) {
+    errorDisplay("lastName","Le Nom ne doit pas contenir de caractères spéciaux");
+    firstName = null;
+  } else { 
+    errorDisplay("lastName", "");
+    firstName = value;
+  }
+};
+const addressChecker = (value) => {
+  if (value.length > 0 && (value.length < 10 || value.length > 40)) {
+    errorDisplay("address", "L'adresse doit faire entre 10 et 40 caractères");
+    address = null;
+  } else if (!value.match(/^[a-zA-Z0-9 _.-]*$/)) {
+    errorDisplay("address","L'adresse ne doit pas contenir de caractères spéciaux");
+    address = null;
+  } else {
+    errorDisplay("address", "")
+    address = value;
+  }
+};
+const cityChecker = (value) => {
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("city", "Le nom de la ville doit faire entre 3 et 20 caractères");
+    city = null;
+  } else if (!value.match(/^[0-9]{5,}[a-zA-Z _.-]*$/)) {
+    errorDisplay("city","Indiquer le nom de la ville avec le code postal et sans caractéres spéciaux");
+    city = null;
+  } else {
+    errorDisplay("city", "")
+    city = value;
+  }
+};
+const emailChecker = (value) => {
+  if (!value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i)) {
+    errorDisplay("email", "Le mail n'est pas valide");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
+};
+
+/*
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (pseudo && email && password && confirmPass) {
+    const data = {
+      pseudo,
+      email,
+      password,
+    };
+    console.log(data);
+
+    inputs.forEach((input) => (input.value = ""));
+    progressBar.classList = "";
+
+    pseudo = null;
+    email = null;
+    password = null;
+    confirmPass = null;
+    alert("Inscription validée !");
+  } else {
+    alert("veuillez remplir correctement les champs");
+  }
+});*/
+
 
